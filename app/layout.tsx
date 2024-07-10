2c1e4129fe63b50a318258d1e914edf8da1d541b
AnmolPlayzz <74979911+AnmolPlayzz@users.noreply.github.com>
2024-07-10 13:18:22 +0530

    v0.2.1 - analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { MdMessage } from "react-icons/md";
 import Link from "next/link";
 import Providers from "@/components/post/ProgressBarProvider";
@@ -59,6 +61,8 @@ export default function RootLayout({
       <Providers>
         {children}
       </Providers>
+      <Analytics />
+      <SpeedInsights />
       </body>
     </html>
   );
